1a74c19e1c4413f920498603c141b51b63d87082
Charm Maria Dacera Legaspi <Chamyko12345@gmail.com>
2016-02-10 12:35:11 +0800

    Initialize Commit

---
+++ app/controllers/auths_controller.rb
@@ -0,0 +1,19 @@
+class AuthsController < ApplicationController
+  def login
+
+  end
+  def verify
+    user = User.find_by(email: params[:user][:email].downcase)
+    if user && user.authenticate(params[:user][:password])
+      log_in user
+      redirect_to root_path
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'login'
+    end
+  end
+  def logout
+    session.destroy
+    redirect_to root_path
+  end
+end

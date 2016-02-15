67cdec2c8ef0ef142662baa09de7e5d115b747b8
Charm Maria Dacera Legaspi <Chamyko12345@gmail.com>
2016-02-15 16:38:22 +0800

    Make Changes

---
+++ app/assets/javascripts/costume.js
@@ -0,0 +1,12 @@
+/**
+ * Created by MICP on 2/15/2016.
+ */
+jQuery.noConflict();
+(function($){
+    $(function(){
+        $(".model-books").click(function(){
+            $("#library-modal").modal()
+        });
+        //alert ("Hello");
+    });
+})(jQuery);
